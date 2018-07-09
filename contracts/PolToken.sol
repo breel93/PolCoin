bb283eb6fd090a29d7ef7f737a7d60c4790bf90b
breezil <kolajubril@gmail.com>
2018-07-09 16:09:44 -0400

    delegated transfer

--- contracts/PolToken.sol
+++ contracts/PolToken.sol
@@ -17,7 +17,17 @@ contract PolToken {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+
+    );
+    
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
     
 
 	function PolToken (uint256 _initialSupply) public {
@@ -37,6 +47,33 @@ contract PolToken {
         Transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    //approve
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+       //allowance
+       allowance[msg.sender][_spender] = _value;
+
+        Approval(msg.sender,_spender, _value);
+        return true;
+    }
+
+
+    function transferFrom(address _from,address _to, uint256 _value) public returns(bool success){
+
+
+        require (_value <= balanceOf[_from]);
+        require (_value <= allowance[_from][msg.sender]);
+        
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;   
     }
 
 }
