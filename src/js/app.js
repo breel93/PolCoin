62662c9c27d28723d86a5ea8a3d00b8a406904b4
breezil <kolajubril@gmail.com>
2018-07-24 15:16:02 -0400

    load token sale contract

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,10 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
-
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
 
 
   init: function(){
@@ -37,22 +40,96 @@ App = {
         App.contracts.PolToken.deployed().then(function(polToken) {
           console.log("Pol Token Sale Address:", polToken.address);
         });
+        App.listenForEvents();
         return App.render();
       });
     });
   
   },
 
+   // Listen for events emitted from the contract
+   listenForEvents: function() {
+    App.contracts.PolTokenSale.deployed().then(function(instance) {
+      instance.Sell({}, {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+      })
+    })
+  },
+
   // Load account data
-  render: function(){
-    web3.eth.getCoinbase(function(err, account){
-      if(err === null){
+  render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader  = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
         App.account = account;
-        $('#accountAddress').html("Your Account:" + account);
+        $('#accountAddress').html("Your Account: " + account);
       }
     })
+
+    // Load token sale contract
+    App.contracts.PolTokenSale.deployed().then(function(instance) {
+      polTokenSaleInstance = instance;
+      return polTokenSaleInstance.tokenPrice();
+    }).then(function(tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+      return polTokenSaleInstance.tokensSold();
+    }).then(function(tokensSold) {
+      App.tokensSold = tokensSold.toNumber();
+      $('.tokens-sold').html(App.tokensSold);
+      $('.tokens-available').html(App.tokensAvailable);
+
+      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+      $('#progress').css('width', progressPercent + '%');
+
+      // Load token contract
+      App.contracts.PolToken.deployed().then(function(instance) {
+        polTokenInstance = instance;
+        return polTokenInstance.balanceOf(App.account);
+      }).then(function(balance) {
+        $('.dapp-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      })
+    });
+  },
+
+
+  buyTokens: function() {
+    $('#content').hide();
+    $('#loader').show();
+    var numberOfTokens = $('#numberOfTokens').val();
+    App.contracts.PolTokenSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
+        from: App.account,
+        value: numberOfTokens * App.tokenPrice,
+        gas: 500000 // Gas limit
+      });
+    }).then(function(result) {
+      console.log("Tokens bought...")
+      $('form').trigger('reset') // reset number of tokens in form
+      // Wait for Sell event
+    });
   }
 }
+
+
 
 $(function(){
   $(window).load(function(){
